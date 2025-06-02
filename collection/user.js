d46dbdddfdb282000382154510c1edafbfb53837
Darshit1101 <darshitkakadiya1@gmail.com>
2025-06-02 16:59:46 +0530

    update

--- collection/user.js
+++ collection/user.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
+// Define the User model
 const User = sequelize.define('User', {
   fullName: {
     type: DataTypes.STRING,
