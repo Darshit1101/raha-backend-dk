3e3009b25001950fe7c1c6835bb7b505fdbfa50a
Darshit1101 <darshitkakadiya1@gmail.com>
2025-06-04 15:06:38 +0530

    update

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,5 @@
-const router = express.Router();
 const Ctrl = require('../controller/user');
+const app = module.exports = express.Router();
 
-router.post('/register', Ctrl.registerUser);
-router.post('/login', Ctrl.loginUser);
-
-module.exports = router;
+app.route(env.API_PREFIX + '/register').post(Ctrl.registerUser);
+app.route(env.API_PREFIX + '/login').post(Ctrl.loginUser);
--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 require('./config/global'); // Load all globals (sequelize, app, env, etc.)
-require('./model/index');  
-// require('./config/route');
-const userRoutes = require('./routes/user'); // Import user routes
-
-// Register API routes
-app.use('/api', userRoutes);
+require('./model/index');  // Load all models
+require('./config/route');// Load routes
 
 const PORT = process.env.PORT || 5000;
 
