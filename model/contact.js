6f64ed04163421b6973197cbdaff58ad53663ab8
Darshit Kakadiya <darshitkakadiya1@gmail.com>
2025-06-05 12:56:29 +0530

    update

--- model/contact.js
+++ model/contact.js
@@ -1,33 +1,39 @@
-const Contact = sequelize.define("Contact", {
-  contactId: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Contact = sequelize.define(
+  "contacts",
+  {
+    contactId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.ENUM(
+        "Product Inquiry",
+        "Order Support",
+        "#BADLAV Movement"
+      ),
+      allowNull: false,
+    },
+    message: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  phone: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  type: {
-    type: DataTypes.ENUM(
-      "Product Inquiry",
-      "Order Support",
-      "#BADLAV Movement"
-    ),
-    allowNull: false,
-  },
-  message: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-});
+  {
+    tableName: "contacts",
+  }
+);
 
 module.exports = Contact;
--- model/user.js
+++ model/user.js
@@ -1,35 +1,41 @@
-const User = sequelize.define('User', {
-  userId: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const User = sequelize.define(
+  "user",
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: { name: "unique_email", msg: "Email must be unique" },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("user", "admin"),
+      allowNull: false,
+      defaultValue: "user",
+    },
+    token: {
+      type: DataTypes.STRING,
+      defaultValue: null,
+    },
   },
-  fullName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: { name: "unique_email", msg: "Email must be unique" },
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.ENUM("user", "admin"),
-    allowNull: false,
-    defaultValue: "user",
-  },
-  token: {
-    type: DataTypes.STRING,
-    defaultValue: null,
-  },
-});
+  {
+    tableName: "user",
+  }
+);
 
 module.exports = User;
