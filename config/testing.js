9d7d96e35670557fbf390016bda5a137698314e1
Darshit Kakadiya <darshitkakadiya1@gmail.com>
2025-06-16 16:23:27 +0530

    Update testing.js

--- config/testing.js
+++ config/testing.js
@@ -8,5 +8,5 @@ function sum(...nums) {
   return nums.reduce((acc, curr) => acc + curr);
 }
 
-console.log('Sum =', sum(10, 2, 3));       
-console.log('Multiply =', multiply(1, 2, 3)); 
+// console.log('Sum =', sum(10, 2, 3));       
+// console.log('Multiply =', multiply(1, 2, 3)); 
