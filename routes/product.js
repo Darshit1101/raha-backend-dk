81d4d7537ecc7609fff28de80be63ab20a67e0b4
Darshit Kakadiya <darshitkakadiya1@gmail.com>
2025-06-16 14:09:40 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -2,13 +2,8 @@ const Ctrl = require("../controller/product");
 const app = (module.exports = express.Router());
 const upload = require("../service/multer");
 
-app
-  .route(env.API_PREFIX + "/addProduct")
-  .post(upload.array("image"), Ctrl.addProduct);
-
-app
-  .route(env.API_PREFIX + "/updateProduct/:id")
-  .put(upload.array("image"), Ctrl.updateProduct);
+app.route(env.API_PREFIX + "/addProduct").post(upload.array("image"), Ctrl.addProduct);
+app.route(env.API_PREFIX + "/updateProduct/:id").put(upload.array("image"), Ctrl.updateProduct);
 
 app.route(env.API_PREFIX + "/deleteProduct/:id").delete(Ctrl.deleteProduct);
 app.route(env.API_PREFIX + "/getAllProducts").get(Ctrl.getAllProducts);
