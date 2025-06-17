a959677be41bff4a14a2bf2d2fc7424f68e0fe72
Darshit Kakadiya <darshitkakadiya1@gmail.com>
2025-06-17 15:58:40 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -6,7 +6,12 @@
 // commonJS ==> syncronous module 
 // const fs = require('fs');
 // module.exports = myFunction;
+// Top level await is not allowed.
+// Only one value can be exported in commonJS.
 
 // ES Modules ==> Asynchronous module
 // import fs from 'fs';
 // export default myFunction;
+// we have to set "type": "module" in package.json to use ES Modules
+// Top level await is allowed in ES Modules.
+// Multiple value can be exported in ES Modules.
